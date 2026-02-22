a09864fa7398cfd2910adc1c847428f7f83162c6
xodo2fast4u <officialrtskhosana@gmail.com>
2026-02-22 11:38:57 +0200

    feat: add about command

---
+++ src/commands/general/about.js
@@ -0,0 +1,22 @@
+import { config, emoji as e } from "../../config/config.js";
+import { send } from "../../utils/utils.js";
+
+export default {
+  cmd: ["about"],
+  desc: "About this bot",
+
+  run: async (sock, msg) => {
+    await send.text(
+      sock,
+      msg,
+      `
+╭━━━ ${e.sonic} *ABOUT ${config.botName.toUpperCase()}* ━━━╮
+┃ ${e.star} Name: ${config.botName}
+┃ ${e.rocket} Version: ${config.version}
+┃ ${e.info} A WhatsApp bot with economic features
+┃ ${e.bolt} Fast, reliable & feature-rich
+┃ ${e.crown} Owner: @${config.ownerNumber}
+╰━━━━━━━━━━━━━━━━━━━━━━━━━━━╯`.trim(),
+    );
+  },
+};
